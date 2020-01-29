e556d6e8a7794c6b3afb21226dd54f978fe2819e
amarode <amarod@post.jce.ac.il>
2020-01-29 13:05:30 +0200

    Code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Peer-graded Assignment: Bash, Make, Git, and Github #
 
-Tue Jan 28 19:37:51 IST 2020
+Wed Jan 29 12:09:58 IST 2020
 
 Number of lines in Guessinggame: 
-10
+34
--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,34 @@
 #!/usr/bin/env bash
 # File: guessinggame.sh
 
-echo "Hi user, How many files do you think are in my directory?"
-echo "please enter your guessing number"
+filesCount=$(ls | wc -l)
 
-#
-#
-#
-#
+function compareGuess {
+	if [[ $1 -eq $2 ]]; then
+		echo "Congratulation, Your guess is correct. BYE"
+                return 0
+        elif [[ $1 -gt $2 ]]; then
+		echo "Your guess is bigger than the correct number."
+		return 1
+        else
+		echo "Your guess is smaller than the correct number."
+		return 1
+	fi
+}
+
+echo ""
+echo "Hi user, How many files do you think are in my directory?"
+echo "Please enter your guessing number"
+echo ""
+read guess
+echo ""
+echo "You have guessed $guess"
+
+while ! compareGuess $guess $filesCount;
+	do
+		echo "Please try again."
+		echo ""
+		read guess
+		echo ""
+	done
+exit 0
